e0350a582fadd8c9a30e0b3e90de7389bc4fe433
Alice <alice.calderini@gmail.com>
2026-09-04 19:14:50 +0200

    final refinements

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,5 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
-  // Cambia questo con il nome del tuo repository GitHub se pubblichi su GitHub Pages
-  // (es. se il repo si chiama "cootefoo-visual-analytics", base deve essere '/cootefoo-visual-analytics/').
-  // In sviluppo locale (npm run dev) questo valore non ha effetto.
   base: '/Visual_Analytics_Project/',
 })
